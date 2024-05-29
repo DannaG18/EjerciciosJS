2995df1d6a3c716cf526913292041bc1b4f25a49
DannaG18 <158192981+DannaG18@users.noreply.github.com>
2024-05-28 21:01:40 -0500

    End :sparkles:

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 do{
-    options = prompt ("1. Valid Password\n2. Calculate your incomes\n3. Calculate your IMC\n4. Print Arrangement\n5. Count Range\n6. Likes ")
+    options = prompt ("1. Valid Password\n2. Calculate your incomes\n3. Calculate your IMC\n4. Print Arrangement\n5. Count Range\n6. Likes\n7.  Sum ranges\n8. Number of a's\n9. Sum Arrangement\n10. Multiply Arrangement\n11. Remove Ceros\n12. Transcribe\n13. ")
     switch(options) {
         case ("1"):
             password = prompt ("Enter your password")
@@ -23,15 +23,56 @@ do{
             console.log(contarRango(5, 6));
             break;
         case ("6"):
-            console.log(likes(983));
-            console.log(likes(1983));
-            console.log(likes(54000));
-            console.log(likes(120800));
+            console.log(likes(983)) // "983"
+            console.log(likes(1900)) // "1K"
+            console.log(likes(54000)) // "54K"
+            console.log(likes(120800)) // "120K"
+            console.log(likes(25222444)) // "25M"
             break;
         case ('7'):
-            console.log(sumarRango(0, 10));
-            console.log(sumarRango(12, 14));
-            console.log(sumarRango(5, 5));
+            console.log(sumarRango(0, 10)) // 55
+            console.log(sumarRango(12, 14)) // 39
+            console.log(sumarRango(5, 5)) // 0
+            break
+        case('8'):    
+            console.log(numeroDeAes("abracadabra")) // 5
+            console.log(numeroDeAes("etinol")) // 0
+            console.log(numeroDeAes("a")) // 0
+        case('9'):
+
+            console.log(sumarArreglo([3, 1, 2])) // 6
+            console.log(sumarArreglo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // 55
+            console.log(sumarArreglo([])) // 0
+            break
+        case('10'):
+            console.log(multiplicarArreglo([4, 1, 2, 3])) // 24
+            console.log(multiplicarArreglo([1, 2, 3, 4, 5, 6, 7, 8])) // 40320
+            console.log(multiplicarArreglo([])) // 1
+        case('11'):
+            console.log(removerCeros([0, 1, 0, 2, 0, 3]));
+            console.log(removerCeros([9, 3, 6, 4]));
+            console.log(removerCeros([0, 0 , 0]));
+        case('12'):
+            console.log(transcribir("ACGT")) // "UGCA"
+            console.log(transcribir("ACGTGGTCTTAA")) // "UGCACCAGAAUU"
+        case('13'):
+            console.log(capitalizar("pedro")) // "Pedro"
+            console.log(capitalizar("hola mundo")) // "Hola mundo"
+            console.log(capitalizar("")) // ""
+        case('14'):
+            console.log(max([3, 9, 6])) // 9
+            console.log(max([67, 35, 54, 26])) // 67
+            console.log(max([5, 9, 2, 4, 5, 7])) // 9
+        case('15'):
+            console.log(pares([1, 2, 3, 4, 5, 6])) // [2, 4, 6]
+            console.log(pares([])) // []
+        case('16'):
+            console.log(password("hola")) // "h0l4"
+            console.log(password("esta es una prueba")) // "3st43sun4pru3b4"
+            console.log(password("")) // ""
+        case('17'):
+            console.log(posiciones([1, 2, 3, 4, 5, 6])) // [1, 3, 5]
+            console.log(posiciones([])) // []
     }
 }while (options)
 
@@ -86,7 +127,7 @@ function imprimirArreglo(f1,f2,f3,f4) {
 
 //EJERCICIO 5
 
-    const contarRango = (n1, n2) => {
+    function contarRango(n1, n2) {
     let counter = 0;
     for (n1; n1<n2; ++n1) {
         counter++;
@@ -111,7 +152,7 @@ function likes (number) {
 
 //EJERCICIO 7
 
-function sumarRangos (inicial, final){
+function sumarRango (inicial, final){
     let suma = 0
     for (inicial; inicial <= final; ++inicial){
         suma += inicial
@@ -121,7 +162,7 @@ function sumarRangos (inicial, final){
 
 //EJERCICIO 8
 
-const numeroDeAes = (word) => {
+function numeroDeAes(word) {
     let counter = 0;
     for (let i = 0; i < word.toLowerCase().length; ++i) {
       if (word[i] === 'a') {
@@ -130,3 +171,153 @@ const numeroDeAes = (word) => {
     }
     return counter;
   }
+
+//EJERCICIO 9
+
+function sumarArreglo(num){
+    try {
+        return num.reduce((a,b) => a + b);
+    }catch{
+        return 0;
+    }
+}
+
+//EJERCICIO 10
+
+function multiplicarArreglo(num){
+    try {
+        return num.reduce((a,b) => a * b);
+    }catch{
+        return 0;
+    }
+}
+
+
+//EJERCICIO 11
+
+function removerCeros(num){
+    try {
+        return num.filter((number) => number != 0);
+    }catch (error){
+        return [];
+    }
+    }
+
+//EJERCICIO 12
+
+function transcribir(string){
+
+    let ARN = [];
+    for (let i = 0; i < ARN.toUpperCase().length; ++i) {
+      const ADN = ARN[i].toUpperCase();
+      switch (ADN) {
+        case 'G':
+          ARN.push('C');
+          break;
+        case 'C':
+          ARN.push('G');
+          break;
+        case 'T':
+          ARN.push('A');
+          break;
+        case 'A':
+          ARN.push('U');
+        default:
+          continue;
+      }
+    }
+    return ARN;
+  }
+
+  //EJERCICIO 13
+
+  const capitalizar = (word) => {
+    if (frase[0] != null) {
+        const mayuscula = word[0].toUpperCase();
+        const out = mayuscula + word.substring(1);
+        return out;
+    }
+    else {return ""};
+  };
+
+  //EJERCICIO 14
+
+function max (arreglo){
+    if (arreglo.length === 0) {
+        return undefinded;
+    }
+    let maxNumber = arreglo[0];
+
+    for (let i=1; i < arreglo.length; i++){
+        if (arreglo[i] > maxNumber){
+            maxNumber = arreglo[i];
+        }
+    }
+    return maxNumber;
+}
+
+//EJERCICIO 15
+
+function pares(numbers){
+    const paresNumbers = [];
+    for (let i= 0; i < numbers.length; i++);{
+        if (numbers[i] % 2 === 0 ) {
+            paresNumbers.push(numbers[i]);
+    }
+}
+return paresNumbers;
+}
+
+//EJERCICIO 16
+
+function password (text) {
+
+    text = text.toLowerCase();
+    
+    text = removeWhiteSpace(text);
+
+    const toChar = str.split('');
+
+     for (let i = 0; i < toChar.length; i++) {
+        switch (toChar[i]){
+            case 'a':
+                toChar[i] = '4';
+                break;
+            case'e':
+                toChar[i] = '3';
+                break;
+            case'i':
+                toChar[i] = '1';
+                break;
+            case'o':
+                toChar[i] = '0';
+                break;
+        }
+     }
+     str = toChar.join('');
+     return str;
+}
+
+const removeWhiteSpace = (text) => {
+    let result = '';
+    for ( let i=0; i < text.length; i++) {
+        if (str[i] != ' '){
+            result += str[i];
+        }    
+    }
+    return result;
+}
+
+//EJERCICIO 17
+
+function posiciones(arr) {
+    const posicionesPares = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            posicionesPares.push(i); // Si el número es par, agregamos su posición al nuevo arreglo
+        }
+    }
+
+    return posicionesPares;
+}
